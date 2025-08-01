31ba672d4b0903366d622947f57815d5d9341d94
32manish35 <dhurandharmanishkumar@gmail.com>
2025-08-01 12:24:35 +0530

    loop

---
+++ factorial.js
@@ -0,0 +1,17 @@
+const num = 10;
+
+const factorialCalculator = (number) => {
+  let result = 1;
+
+  for (let i = 1; i <= number; i++) {
+    result = result * i;
+  }
+
+  return result;
+};
+
+const factorial = factorialCalculator(num);
+
+const resultMsg = `Factorial of ${num} is ${factorial}`;
+
+console.log(resultMsg);
