f9fda283d6dc8d07c64339de6199a009a9717a3d
32manish35 <dhurandharmanishkumar@gmail.com>
2025-08-04 21:33:37 +0530

    s

---
+++ stringrepeater.js
@@ -0,0 +1,16 @@
+const repeatStringNumTimes = (string,number) =>{
+  let string1 = ""
+for(let i = 0;i<number;i++){
+  if(number<=0){
+    return ""
+  }
+  else{
+    
+    string1 += string
+      
+    
+     }
+     
+}
+return string1
+}
