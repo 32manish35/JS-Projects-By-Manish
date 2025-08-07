4aaacef38509c0daaa9be47a63e9ca1bf9862874
32manish35 <dhurandharmanishkumar@gmail.com>
2025-08-07 16:08:00 +0530

    FirstElementFinder

---
+++ firstElementFinder.js
@@ -0,0 +1,10 @@
+const findElement = (arr,func) =>{
+  
+  for(let i = 0;i<arr.length;i++){
+    let element = arr[i]
+    if(func(element)){
+      return element
+    }
+    }
+    return undefined
+}
