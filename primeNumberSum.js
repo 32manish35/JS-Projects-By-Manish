05fda23a4399f6c6407d43756bf9c3b27d06696d
Manish Kumar Dhurandhar <dhurandharmanishkumar@gmail.com>
2025-11-02 12:28:56 +0530

    Create primeNumberSum.js

---
+++ primeNumberSum.js
@@ -0,0 +1,28 @@
+function sumPrimes(num) {
+  if (num < 2) {
+    return 0;
+  }
+
+  let sum = 0;
+
+  for (let i = 2; i <= num; i++) {
+    let isPrime = true;
+
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+
+    if (isPrime === true) {
+      sum = sum + i;
+    }
+  }
+
+  return sum;
+}
+
+console.log(sumPrimes(10));
+console.log(sumPrimes(5));
+console.log(sumPrimes(1));
